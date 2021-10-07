46870ac1e1345748742f7f583bef69f3ede21abc
Parker Bolick <bolickparker@gmail.com>
2021-10-07 12:53:28 -0400

    Added basic routing

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const routes = require('./routes');
 
 const { environment } = require("./config");
 const isProduction = environment === "production";
@@ -12,6 +13,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
+
 
 // Security Middleware
 if (!isProduction) {
@@ -33,3 +35,5 @@ app.use(
     },
   })
 );
+app.use(routes);
+module.exports = app;
